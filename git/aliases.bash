5098edc11f5b8bd683a52359f59ff64897f810f1
talum <tracy.a.lum@gmail.com>
2018-03-03 21:57:35 -0500

    clean up git aliases

--- git/aliases.bash
+++ git/aliases.bash
@@ -5,26 +5,16 @@ then
   eval "$(hub alias -s)"
 fi
 
-# The rest of my fun git aliases
+
+alias gb="git branch"
+alias gc="git commit -v"
+alias gcam="git commit -am"
+alias gcb='git copy-branch-name'
+alias gcl="git clone"
+alias gco='git checkout'
+alias gd='git diff'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gcb='git copy-branch-name'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gac='git add -A && git commit -m'
-
-#from flatiron
-alias gcl="git clone"
-alias gst="git status"
-alias gl="git pull"
 alias gp="git push"
-alias gd="git diff | mate"
-alias gc="git commit -v"
-alias gca="git commit -v -a"
-alias gb="git branch"
-alias gba="git branch -a"
-alias gcam="git commit -am"
-alias gbb="git branch -b"
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gst="git status"
