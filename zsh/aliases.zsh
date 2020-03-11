60cc5b7f5bf8ce9e5af6a1a2904ac1e2cabc8569
talum <tracy.lum@vimeo.com>
2020-03-11 11:22:23 -0400

    update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,10 +4,11 @@ alias rc='rails c'
 alias rs='rails s'
 alias migrate='rake db:migrate && rake db:migrate RAILS_ENV=test'
 
-# FS
 
 alias l='ls -a'
 alias desktop='cd /Users/$USER/Desktop/$@'
+
+# FS
 alias ironboard='cd /Users/$USER/Development/ironboard'
 alias ib='cd /Users/$USER/Development/ironboard'
 alias phe='cd /Users/$USER/Development/phoeyonce'
@@ -16,6 +17,9 @@ alias dev='cd /Users/$USER/Development'
 alias dotfiles='cd /Users/$USER/.dotfiles'
 alias cssh='i2cssh -c'
 alias dc='docker-compose up'
+
+# Vimeo
+alias crystal='cd /Users/$USER/development/crystal'
 
 #Pubkey
 
